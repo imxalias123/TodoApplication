01c1939be6c4cfef01ec0ad0e850f151277215c6
sailaxmi <sailaxmikomirineni@gmail.com>
2023-06-23 07:28:05 +0000

    solution

--- app.js
+++ app.js
@@ -78,3 +78,53 @@ app.get("/todos/:todoId/", async (request, response) => {
   const data = await db.get(getTodoQuery);
   response.send(data);
 });
+
+app.post("/todos/", async (request, response) => {
+  const { id, todo, priority, status } = request.body;
+  const postTodoQuery = `
+    INSERT INTO todo (id, todo, priority, status)
+    VALUES (${id}, '${todo}', '${priority}', '${status}');
+    `;
+  await db.run(postTodoQuery);
+  response.send("Todo Successfully Added");
+
+  app.put("/todos/:todoId/", async (request, response) => {
+    const { todoId } = request.params;
+    const requestBody = request.body;
+    let updateColumn = "";
+
+    switch (true) {
+      case requestBody.status !== undefined:
+        updateColumn = "Status";
+        break;
+      case requestBody.priority !== undefined:
+        updateColumn = "Priority";
+        break;
+      case requestBody.todo !== undefined:
+        updateColumn = "Todo";
+        break;
+    }
+
+    const previousTodoQuery = `
+  SELECT * FROM todo WHERE id = ${todoId};`;
+    const previousTodo = await db.get(previousTodoQuery);
+
+    const updateTodoQuery = `
+  UPDATE todo 
+  SET 
+  todo = '${todo}',
+  priority = '${priority},
+  status = '${status}'
+  WHERE id = ${todoId};`;
+    await db.run(updateTodoQuery);
+    response.send(`${updateColumn} updated`);
+});
+
+app.delete("/todos/:todoId/", async(request,response) => {
+    const { todoId } = request.params;
+    const deleteTodoQuery = 
+    ` DELETE * FROM todo WHERE id = ${todoId};`;
+    await db.run(deleteTodoQuery);
+    response.send("Todo Deleted")
+});
+module.exports = app;
