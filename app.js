69dd777b3b837ef6c885eb2cbe536185c37685f9
sailaxmi <sailaxmikomirineni@gmail.com>
2023-06-23 07:37:04 +0000

    solution

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 const express = require("express");
-const app = express();
-const sqlite3 = require("sqlite3");
-const { open } = require("sqlite");
 const path = require("path");
-
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
 app.use(express.json());
 const dbPath = path.join(__dirname, "todoApplication.db");
+
 let db = null;
-const initializeDBAndResponse = async () => {
+const initializeDBandServer = async () => {
   try {
     db = await open({
-      fileName: dbPath,
+      filename: dbPath,
       driver: sqlite3.Database,
     });
     app.listen(3000, () => {
-      console.log("Server is running at http://localhost:3000");
+      console.log(`Server Running at http://localhost:3000/`);
     });
   } catch (e) {
     console.log(`DB Error: ${e.message}`);
     process.exit(1);
   }
 };
-
-initializeDBAndResponse();
+initializeDBandServer();
 
 const hasStatusAndPriorityProperty = (requestQuery) => {
   return (
